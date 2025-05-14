00c05c33792db6e631bb5d1863ff75e1a5183ccd
Ruma Das <ruma@neo-analytics.com,au>
2025-05-14 23:07:24 +0930

    "adding a js file"

---
+++ Day_1/js/myscript.js
@@ -0,0 +1,7 @@
+console.log("Hello world");
+const name = "Ruma Das";
+const age = 50;
+const goal = "Freelance Web Developer";
+console.log(
+  ` Hi my name is ${name} and my age is ${age} and i want to become ${goal} in 4 to 5 weeks `
+);
